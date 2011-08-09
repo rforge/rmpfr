0e248505b166c6919aca4baa9c5a679b7e930af0
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-09 15:43:33 +0000

    use a primitive NAMESPACE, inkl useDynLib
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmpfr@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++
@@ -1,7 +0,0 @@
-.First.lib <- function(lib, pkg) {
-    library.dynam("Rmpfr",pkg,lib)
-    if(FALSE) ## does not make sense: is *not* in package subsequently;  need assign() / Namespace ??
-    Pi <- Const("pi")
-    ## for *stable() -- for the moment -- but this *still* fails here.. methods are not yet "active"
-    ## pi2 <- Const("pi", 256) / 2
-}
